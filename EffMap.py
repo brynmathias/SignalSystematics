7f919277d5026cf37cebaca4b6ef9f6699fb33d3
Sam Rogerson <samr@cern.ch>
2012-10-25 16:38:17 +0100

    individual ht plots, all j+b combos

--- EffMap.py
+++ EffMap.py
@@ -4,9 +4,31 @@ from plottingUtils import GetSumHist, Print, threeToTwo
 import ROOT as r
 models = [ 'T2' ] #'T2bb', 'T1bbbb', 'T1']
 
+
+selection = [ 'had', 'muon' ][0]
 histoDirPrefix = 'smsScan'
-histoDirFmt = '{prefix}_{sel}_AlphaT55_{htRange}'
-rootFileFmt = "rootFiles/sigScan_{model}_had_2012_{jPt}_bt0.0_MChi-1.0.root"
+histoDirFmt = '{prefix}_{jb}_AlphaT55_{htRange}'
+rootFileFmt = "rootFiles/sigScan_{model}_{sel}_2012_{jPt}_bt0.0_MChi-1.0.root"
+
+bTagBins = [
+        ('n_{b}=0', 'eq0b'),
+        ('n_{b}=1', 'eq1b'),
+        ('n_{b}=2', 'eq2b'),
+        ('n_{b}=3', 'eq3b'),
+        ('n_{b}#geq4', 'ge4b'),
+        ]
+
+jetBins = [
+        ('n_{j}#leq3', 'le3j'),
+        ('n_{j}#geq4', 'ge4j'),
+        ]
+
+j1 = [(', '.join([b[0], jetBins[0][0]]), '_'.join([b[1],jetBins[0][1]])) for b in bTagBins[:-1]]
+j2 = [(', '.join([b[0], jetBins[1][0]]), '_'.join([b[1],jetBins[1][1]])) for b in bTagBins]
+binCombinations = j1+j2
+
+htBins = [ ('375'), ('275','325'), ('325','375'), ('375','475'), ('475','575'),
+        ('575','675'), ('675','775'), ('775','875'), ('875',) ]
 
 processStamps =  {
     'T2'     : "pp #rightarrow #tilde{q} #tilde{q}, "
@@ -30,70 +52,60 @@ axes = {
         'T2bb': ('m_{sbottom} (GeV)', 'm_{LSP} (GeV)'),
     }
 
-htBins = [ ('275','325'), ('325','375'), ('375','475'), ('475','575'),
-        ('575','675'), ('675','775'), ('775','875'), ('875',) ]
-
-JetBins = {
-    'T2': {
-        'n_{b}=1, n_{j}#geq4': ('eq1b','ge4j'),
-        'n_{b}=2, n_{j}#geq4': ('eq2b','ge4j'),
-        },
-    'T2bb': {
-        'n_{b}=1, n_{j}#leq3': ('eq1b', 'le3j'),
-        },
-    'T1': {
-        'n_{b}=0, n_{j}#geq4': ('eq0b', 'ge4j'),
-        'n_{b}=1, n_{j}#geq4': ('eq1b', 'ge4j'),
-        },
-    'T1bbbb': {
-        'n_{b}=2, n_{j}#geq4': ('eq2b', 'ge4j'),
-        'n_{b}=3, n_{j}#geq4': ('eq3b', 'ge4j'),
-        'n_{b}=4, n_{j}#geq4': ('eq4b', 'ge4j'),
-        },
-    }
+def setHistoOpts(h, model, title):
+    h.SetTitle('{p}, {t}'.format(p=processStamps[model], t=title))
+    h.GetXaxis().SetTitle(axes[model][0])
+    h.GetYaxis().SetTitle(axes[model][1])
+    h.GetZaxis().SetTitle('')
+    h.GetXaxis().SetTitleOffset(h.GetXaxis().GetTitleOffset()*1.3)
+    h.GetYaxis().SetTitleOffset(h.GetYaxis().GetTitleOffset()*1.3)
+    h.GetXaxis().SetTitleSize(0.04)
+    h.GetYaxis().SetTitleSize(0.04)
+    h.GetXaxis().SetLabelSize(h.GetXaxis().GetLabelSize()*0.7)
+    h.GetYaxis().SetLabelSize(h.GetYaxis().GetLabelSize()*0.7)
+    h.GetZaxis().SetLabelSize(h.GetZaxis().GetLabelSize()*0.7)
 
 def makeEffMap(model):
     print "------------------------------"
     print "     {m}".format(m=model)
     print "------------------------------"
-    c1 = Print("{model}_EffMap.pdf".format(model = model))
-    c1.canvas.SetLeftMargin(0.12)
-    c1.canvas.SetTopMargin(0.08)
-    noCutFile = r.TFile.Open(rootFileFmt.format(model=model, jPt='100.0'))
+
+    canvas = r.TCanvas()
+    #c1 = Print("{model}_EffMap.pdf".format(model = model))
+    canvas.SetLeftMargin(0.12)
+    canvas.SetTopMargin(0.08)
+    noCutFile = r.TFile.Open(rootFileFmt.format(model=model, jPt='100.0',
+        sel=selection))
     noCuts = noCutFile.Get('smsScan_before/m0_m12_mChi_noweight')
     noCuts = threeToTwo(noCuts)
     noCuts.Draw("COLZ")
-    # c1.Print()
-    for title, jetBin in JetBins[model].iteritems():
+
+    for (title, selBin) in binCombinations:
         Cuts = None
         for htBin in htBins:
             oBin = "100.0"
-            if htBin is (275,325): oBin = "73.7"
-            if htBin is (325,375): oBin = "86.7"
-            histoFile = r.TFile.Open(rootFileFmt.format(model=model, jPt=oBin))
+            if htBin is ('275','325'): oBin = "73.7"
+            if htBin is ('325','375'): oBin = "86.7"
+            histoFile = r.TFile.Open(rootFileFmt.format(model=model, jPt=oBin,
+                sel=selection))
             htBin_dir = histoDirFmt.format(prefix=histoDirPrefix,
-                    sel='_'.join(jetBin), htRange='_'.join(htBin))
+                    jb=selBin, htRange='_'.join(htBin))
             h = histoFile.Get(htBin_dir+"/m0_m12_mChi_noweight")
             h = threeToTwo(h)
             if Cuts is None:
                 Cuts = h.Clone()
             else:Cuts.Add(h)
 
-        Cuts.SetTitle('{p}, {t}'.format(p=processStamps[model], t=title))
-        Cuts.GetXaxis().SetTitle(axes[model][0])
-        Cuts.GetYaxis().SetTitle(axes[model][1])
-        Cuts.GetZaxis().SetTitle('')
-        Cuts.GetXaxis().SetTitleOffset(Cuts.GetXaxis().GetTitleOffset()*1.3)
-        Cuts.GetYaxis().SetTitleOffset(Cuts.GetYaxis().GetTitleOffset()*1.3)
-        Cuts.GetXaxis().SetTitleSize(0.04)
-        Cuts.GetYaxis().SetTitleSize(0.04)
-        Cuts.GetXaxis().SetLabelSize(Cuts.GetXaxis().GetLabelSize()*0.7)
-        Cuts.GetYaxis().SetLabelSize(Cuts.GetYaxis().GetLabelSize()*0.7)
-        Cuts.GetZaxis().SetLabelSize(Cuts.GetZaxis().GetLabelSize()*0.7)
+            setHistoOpts(h,model,title)
+            h.Divide(noCuts)
+            h.Draw('COLZ')
+            canvas.Print('pdfs/{m}_{b}_{s}_{h}.pdf'.format(m=model, b=selBin,
+                s=selection, h='_'.join(htBin)))
+        setHistoOpts(Cuts,model,title)
         Cuts.Divide(noCuts)
-        Cuts.Draw("COLZ")
-        c1.Print()
-    c1.close()
+        Cuts.Draw('COLZ')
+        canvas.Print('pdfs/{m}_{b}_Eff{s}Sum.pdf'.format(m=model, b=selBin,
+            s=selection))
     noCutFile.Close()
 
 if __name__ == '__main__':
