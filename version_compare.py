e0ade305e1df4633c4ba4dcbed29039572e7b8b5
Chris Lucas <chris.lucas@bristol.ac.uk>
2014-10-13 11:52:49 +0100

    small changes

--- version_compare.py
+++ version_compare.py
@@ -6,7 +6,7 @@ from plotDetails import alphaTDict
 
 
 settings = {
-	"model": ["T2cc", "T2_4body", "T2bw_0p75", "T2bw_0p25", "T2tt"][-1],
+	"model": ["T2cc", "T2_4body", "T2bw_0p75", "T2bw_0p25", "T2tt"][1],
 	"HTBins":   ["200_275", "275_325", "325_375", "375_475", "475_575", "575_675", "675_775", "775_875", "875_975", "975_1075", "1075"],
     "jMulti":   ["le3j", "ge4j", "ge2j"][:2],
     "bMulti":   ["eq0b", "eq1b", "eq2b", "eq3b", "ge0b"][:4],
@@ -29,7 +29,9 @@ def getRootDirs(bMulti_="", jMulti_="", sitv_=False):
     return dirs
 
 def compare_versions(bMulti = None, jMulti = None):
-	versions = ["5", "7"]
+	
+	# comparison is: [num, denom]
+	versions = ["15", "9"]
 
 	print bMulti, jMulti
 
@@ -39,10 +41,11 @@ def compare_versions(bMulti = None, jMulti = None):
 
 		centralRootFile100 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_100.0_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
 		centralRootFile87 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_86.7_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
-		if v=="5":
-			centralRootFile73 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_73.7_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
-		elif v=="7":
-			centralRootFile73 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_73.3_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
+		# if v=="5":
+			# centralRootFile73 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_73.7_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
+		# elif v=="7":
+			# centralRootFile73 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_73.3_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
+		centralRootFile73 = r.TFile.Open("./rootFiles/%s_v%s/sigScan_%s_%s_2012_73.3_bt0.0_MChi-1.0.root"%(settings["model"], v, settings["model"], "had"))
 		nocuts = sutils.GetHist(File=centralRootFile100, folder=["smsScan_before", ], hist="m0_m12_mChi_weight", Norm=None, rebinY=1)
 		nocuts = sutils.threeToTwo(nocuts)
 
